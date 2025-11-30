d09b1af5214a8e01324384875843c3f4b084d02d
Mohamed Ghoul <118106616+Mohavvvvd@users.noreply.github.com>
2025-11-30 22:38:48 +0100

    Remove default port fallback in server.js

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const server = app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
