3c6f3f0661ab2ff1d9d3f962bb44b1cab2c486bf
Mohamed Ghoul <118106616+Mohavvvvd@users.noreply.github.com>
2025-12-02 14:09:25 +0100

    Make CORS origin permissive for development
    Changed CORS origin to allow all origins for development.

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use(limiter);
 
 // CORS - More permissive for development
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: "*",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
 }));
